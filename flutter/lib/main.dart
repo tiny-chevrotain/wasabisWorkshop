ce4cc720415f5fc38398680bdea2ad32ce8264b4
Oliver Patrick Gannon <olliepgannon@hotmail.com>
2022-04-11 19:13:32 +0100

    Fixing what i removed

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -58,6 +58,24 @@ List<Wasabia> wasabia_array = wasabias_json
         ))
     .toList();
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        scaffoldBackgroundColor: colour_bckgrnd,
+        brightness: Brightness.dark,
+      ),
+      home: WasabiaListPage(
+        wasabias: wasabia_array,
+      ),
+    );
+  }
+}
